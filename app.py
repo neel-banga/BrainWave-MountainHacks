8288bbf37f0ed3d4818c6daf687248b8396eb908
Vedant Parikh <89679624+vedant-par@users.noreply.github.com>
2023-03-18 19:03:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/audio')
 def audio():
     return render_template('audio.html')
+
 @app.route('/breath')
 def breath():
     return render_template('breath.html')
+    
+
 @app.route('/hand')
 def hand():
     return render_template('hand.html')
 
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
 
